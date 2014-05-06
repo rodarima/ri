2425e249377ad28f7bbbec3cd323df735263f064
Rodrigo Arias <rodarima@gmail.com>
2014-05-07 01:12:10 +0200

    Práctica 3 con consultas simples

--- p3/src/p3/QueryLauncher.java
+++ p3/src/p3/QueryLauncher.java
@@ -37,209 +37,197 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
-/** Simple command-line based search demo. */
-public class SearchFiles
+public class QueryLauncher
 {
-
-	private void usage()
+	private static void usage()
 	{
-		String use = "" +
+		String use = "QueryLauncher: Realiza operaciones de consulta en índices.\n" +
+		"\n"+
+		"Uso del programa:\n"+
+		" java p3.QueryLauncher INDEX [OPTIONS] QUERY\n"+
+		"\n"+
+		"Índice (INDEX):\n"+
+		" -index DIR          Especifica el índice a emplear\n"+
+		"Opciones (OPTIONS):\n"+
+		" -showfield FIELD    Muestra el id, la puntuación y el campo FIELD\n"+
+		" -out DIR            Especifica el lugar donde se almacenará el nuevo índice\n"+
+		"Consulta (QUERY) sólo especificar una:\n"+
+		" -query FIELD QUERY  Resultados de la consulta QUERY sobre el campo FIELD.\n"+
+		" -multiquery {F Q}   Resultados de las consultas sobre su respectivo campo.\n"+
+		" -progquery F -and {T} -or {T} -not {T} Realiza una consulta empleando los\n"+
+		"                     operadores lógicos con los términos especificados.\n"+
+		"\n"+
+		"Ejemplos:\n"+
+		" java p3.QueryLauncher index/ -showfield content -query content fish\n"+
+		" java p3.QueryLauncher index/ -query content \"tropical fish\"\n"+
+		" java p3.QueryLauncher index/ -multiquery content tropical content fish\"\n"+
+		" java p3.QueryLauncher index/ -progquery content -and tropical fish -or trees\n"+
+		"   -not water\"\n"+
+		"\n"+
+		"";
+		System.out.print(use);
 	}
 
-	private SearchFiles() {}
-
-	/** Simple command-line based search demo. */
-	public static void main(String[] args) throws Exception {
-		String usage =
-			"Usage:\tjava org.apache.lucene.demo.SearchFiles [-index dir] [-field f] [-repeat n] [-queries file] [-query string] [-raw] [-paging hitsPerPage]\n\nSee http://lucene.apache.org/java/4_0/demo.html for details.";
-		if (args.length > 0 && ("-h".equals(args[0]) || "-help".equals(args[0]))) {
-			System.out.println(usage);
+	public static void main(String[] args) throws Exception
+	{
+		if (args.length == 0 || "-h".equals(args[0]) || 
+			"-help".equals(args[0]) || "--help".equals(args[0]))
+		{
+			usage();
 			System.exit(0);
 		}
 
-		String index = "index";
-		String field = "contents";
-		String queries = null;
-		int repeat = 0;
-		boolean raw = false;
-		String queryString = null;
-		int hitsPerPage = 10;
+		File file_index = null;
+		File file_out = null;
+		String showfield = null;
+		String out_path = null;
+		Query query = null;
 
-		for(int i = 0;i < args.length;i++) {
-			if ("-index".equals(args[i])) {
-				index = args[i+1];
-				i++;
-			} else if ("-field".equals(args[i])) {
-				field = args[i+1];
-				i++;
-			} else if ("-queries".equals(args[i])) {
-				queries = args[i+1];
-				i++;
-			} else if ("-query".equals(args[i])) {
-				queryString = args[i+1];
-				i++;
-			} else if ("-repeat".equals(args[i])) {
-				repeat = Integer.parseInt(args[i+1]);
-				i++;
-			} else if ("-raw".equals(args[i])) {
-				raw = true;
-			} else if ("-paging".equals(args[i])) {
-				hitsPerPage = Integer.parseInt(args[i+1]);
-				if (hitsPerPage <= 0) {
-					System.err.println("There must be at least 1 hit per page.");
-					System.exit(1);
-				}
-				i++;
-			}
-		}
+		boolean index_status = false;
+		boolean showfield_status = false;
+		boolean out_status = false;
+		boolean query_status = false;
 
-		IndexReader reader = DirectoryReader.open(FSDirectory.open(new File(index)));
-		IndexSearcher searcher = new IndexSearcher(reader);
+		IndexReader reader = null;
+		IndexSearcher searcher = null;
 		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_40);
 
-		BufferedReader in = null;
-		if (queries != null) {
-			in = new BufferedReader(new InputStreamReader(new FileInputStream(queries), "UTF-8"));
-		} else {
-			in = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
-		}
-		QueryParser parser = new QueryParser(Version.LUCENE_40, field, analyzer);
-		while (true) {
-			if (queries == null && queryString == null) {                        // prompt the user
-				System.out.println("Enter query: ");
-			}
 
-			String line = queryString != null ? queryString : in.readLine();
+		for(int i = 0; i<args.length; i++)
+		{
+			String arg = args[i];
 
-			if (line == null || line.length() == -1) {
-				break;
-			}
-
-			line = line.trim();
-			if (line.length() == 0) {
-				break;
-			}
-
-			Query query = parser.parse(line);
-			System.out.println("Searching for: " + query.toString(field));
-
-			if (repeat > 0) {                           // repeat & time as benchmark
-				Date start = new Date();
-				for (int i = 0; i < repeat; i++) {
-					searcher.search(query, null, 100);
+			if("-index".equals(arg))
+			{
+				if(index_status)
+				{
+					System.out.print("Opción \"-index\": Carpeta de índice repetida\n");
+					return;
 				}
-				Date end = new Date();
-				System.out.println("Time: "+(end.getTime()-start.getTime())+"ms");
+				index_status = true;
+				if(!testArgs(args, i, 1)) return;
+				String path = args[i+1];
+				if(!is_readable_file(path))
+				{
+					System.out.print("Opción \"-index\": Carpeta de índice inexistente o ilegible\n");
+					return;
+				}
+				if(!is_dir(path))
+				{
+					System.out.print("Opción \"-index\": No es una carpeta\n");
+					return;
+				}
+				
+				file_index = new File(path);
+				reader = DirectoryReader.open(FSDirectory.open(file_index));
+				searcher = new IndexSearcher(reader);
+				i++;
 			}
+			else if("-showfield".equals(arg))
+			{
+				if(showfield_status)
+				{
+					System.out.print("Opción \"-out\" repetida\n");
+					return;
+				}
+				showfield_status = false;
 
-			doPagingSearch(in, searcher, query, hitsPerPage, raw, queries == null && queryString == null);
-
-			if (queryString != null) {
-				break;
+				if(!testArgs(args, i, 1)) return;
+				showfield = args[++i];
+			}
+			else if("-out".equals(arg))
+			{
+				if(out_status)
+				{
+					System.out.print("Opción \"-out\": Carpeta de índice de salida repetida\n");
+					return;
+				}
+				out_status = true;
+				if(!testArgs(args, i, 1)) return;
+				String path = args[i+1];
+				if(!is_readable_file(path))
+				{
+					System.out.print("Opción \"-out\": Carpeta de índice de salida inexistente o ilegible\n");
+					return;
+				}
+				if(!is_dir(path))
+				{
+					System.out.print("Opción \"-out\": No es una carpeta\n");
+					return;
+				}
+				
+				file_out = new File(path);
+				i++;
+			}
+			else if("-query".equals(arg))
+			{
+				if(query_status)
+				{
+					System.out.print("Opción de consulta ya especificada\n");
+					return;
+				}
+				query_status = true;
+				if(!testArgs(args, i, 2)) return;
+				String field = args[++i];
+				String query_txt = args[++i];
+				QueryParser parser = new QueryParser(Version.LUCENE_40, field, analyzer);
+				query = parser.parse(query_txt);
+				i++;
 			}
 		}
-		reader.close();
+
+
+		if(!index_status)
+		{
+			System.out.print("Opción \"-index\" no especificada\n");
+			return;
+		}
+
+		if(!query_status)
+		{
+			System.out.print("Opción de consulta no especificada\n");
+			return;
+		}
+
+		search(searcher, query);
 	}
 
-	/**
-	 * This demonstrates a typical paging search scenario, where the search engine presents 
-	 * pages of size n to the user. The user can then go to the next page if interested in
-	 * the next hits.
-	 * 
-	 * When the query is executed for the first time, then only enough results are collected
-	 * to fill 5 result pages. If the user wants to page beyond this limit, then the query
-	 * is executed another time and all hits are collected.
-	 * 
-	 */
-	public static void doPagingSearch(BufferedReader in, IndexSearcher searcher, Query query, 
-			int hitsPerPage, boolean raw, boolean interactive) throws IOException {
-
-		// Collect enough docs to show 5 pages
-		TopDocs results = searcher.search(query, 5 * hitsPerPage);
+	private static void search(IndexSearcher searcher, Query query) throws IOException
+	{
+		int hitsPerPage = 50;
+		TopDocs results = searcher.search(query, hitsPerPage);
 		ScoreDoc[] hits = results.scoreDocs;
-
 		int numTotalHits = results.totalHits;
-		System.out.println(numTotalHits + " total matching documents");
-
-		int start = 0;
+		
 		int end = Math.min(numTotalHits, hitsPerPage);
-
-		while (true) {
-			if (end > hits.length) {
-				System.out.println("Only results 1 - " + hits.length +" of " + numTotalHits + " total matching documents collected.");
-				System.out.println("Collect more (y/n) ?");
-				String line = in.readLine();
-				if (line.length() == 0 || line.charAt(0) == 'n') {
-					break;
-				}
-
-				hits = searcher.search(query, numTotalHits).scoreDocs;
-			}
-
-			end = Math.min(hits.length, start + hitsPerPage);
-
-			for (int i = start; i < end; i++) {
-				if (raw) {                              // output raw format
-					System.out.println("doc="+hits[i].doc+" score="+hits[i].score);
-					continue;
-				}
-
-				Document doc = searcher.doc(hits[i].doc);
-				String path = doc.get("title");
-				if (path != null) {
-					System.out.println((i+1) + ". " + path);
-					String title = doc.get("title");
-					if (title != null) {
-						System.out.println("   Title: " + doc.get("title"));
-					}
-				} else {
-					System.out.println((i+1) + ". " + "No path for this document");
-				}
-
-			}
-
-			if (!interactive || end == 0) {
-				break;
-			}
-
-			if (numTotalHits >= end) {
-				boolean quit = false;
-				while (true) {
-					System.out.print("Press ");
-					if (start - hitsPerPage >= 0) {
-						System.out.print("(p)revious page, ");  
-					}
-					if (start + hitsPerPage < numTotalHits) {
-						System.out.print("(n)ext page, ");
-					}
-					System.out.println("(q)uit or enter number to jump to a page.");
-
-					String line = in.readLine();
-					if (line.length() == 0 || line.charAt(0)=='q') {
-						quit = true;
-						break;
-					}
-					if (line.charAt(0) == 'p') {
-						start = Math.max(0, start - hitsPerPage);
-						break;
-					} else if (line.charAt(0) == 'n') {
-						if (start + hitsPerPage < numTotalHits) {
-							start+=hitsPerPage;
-						}
-						break;
-					} else {
-						int page = Integer.parseInt(line);
-						if ((page - 1) * hitsPerPage < numTotalHits) {
-							start = (page - 1) * hitsPerPage;
-							break;
-						} else {
-							System.out.println("No such page");
-						}
-					}
-				}
-				if (quit) break;
-				end = Math.min(numTotalHits, start + hitsPerPage);
-			}
+		for(int i = 0; i < end; i++)
+		{
+			Document doc = searcher.doc(hits[i].doc);
+			System.out.println("Title: " + doc.get("title"));
 		}
+	}
+
+	public static boolean testArgs(String[] args, int i, int n)
+	{
+		if(i+n >= args.length)
+		{
+			System.out.print("Opción \""+args[i]+"\": Faltan argumentos\n");
+			return false;
+		}
+		return true;
+	}
+	private static boolean is_readable_file(String path)
+	{
+		File f = new File(path);
+		if (!f.exists() || !f.canRead())
+		{
+			return false;
+		}
+		return true;
+	}
+	private static boolean is_dir(String path)
+	{
+		File f = new File(path);
+		return f.isDirectory();
 	}
 }
